a1302cd9e2945917df5d32a05ef9d1cf30b12b52
Alex Gartner <alex.gartner@braincorp.com>
2024-03-10 12:46:18 -0700

    fix ci (#1)

---
+++ dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const tc = __importStar(require("@actions/tool-cache"));
+async function run() {
+    const version = core.getInput('version');
+    const downloadUrl = `https://github.com/alexgartner-bc/ci-multitool/releases/download/v${version}/ci-multitool_${version}_linux_amd64.tar.gz`;
+    core.info(`Downloading ci-multitool from ${downloadUrl}`);
+    const localPath = await tc.downloadTool(downloadUrl);
+    const extractedPath = await tc.extractTar(localPath);
+    core.addPath(extractedPath);
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ async function run(): Promise<void> {
   const version = core.getInput('version');
   
   const downloadUrl = `https://github.com/alexgartner-bc/ci-multitool/releases/download/v${version}/ci-multitool_${version}_linux_amd64.tar.gz`
+  core.info(`Downloading ci-multitool from ${downloadUrl}`);
   const localPath = await tc.downloadTool(downloadUrl);
   const extractedPath = await tc.extractTar(localPath);
   core.addPath(extractedPath);
